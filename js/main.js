f66794aafe6bc6c96c83382444228314b9a00a79
Elena <mesheryakova-elena90@yandex.ru>
2021-05-11 11:22:05 +0300

    added forms

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
+
 	$('.header__bottom-burger').click(function() {
         $('.header__bottom-nav').toggleClass('active');
         $('.header__bottom-burger').toggleClass('active');
     });
+
+    
+    $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+    
+    $('.callback__form-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
 });
